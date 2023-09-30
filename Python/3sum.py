2eb44a8fd4d1cbe0aa8f96534872f88c6ae8ed8d
ArpitRupa <ar9cd@virginia.edu>
2023-09-30 00:15:13 -0400

    3sum faster implementation O(n^2)

--- Python/3sum.py
+++ Python/3sum.py
@@ -3,30 +3,74 @@
 # Notice that the solution set must not contain duplicate triplets.
 
 
-# My soltuion time compexity:
-# O(n^3)
-
 from typing import List
 
 
 class ThreeSum:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        # sort given list
+        nums.sort()
+
         # set to store triplets
         all_triplets = set()
 
         # iterate through the list
         for i in range(len(nums)):
-            for j in range(i):
-                for k in range(j):
+            j = 0
+            k = len(nums)-1
+            current_triplet = [nums[i], nums[j], nums[k]]
 
-                    # if indicies are same skip the iteration
-                    if (i == j or i == k or j == k):
-                        continue
+            while (True):
 
-                    # list of current triplets
-                    current_triplet = [nums[i], nums[j], nums[k]]
+                # list of current triplets
+                current_triplet = [nums[i], nums[j], nums[k]]
 
-                    # if sum of the triplet == 0, add to the set
-                    if sum(current_triplet) == 0:
-                        all_triplets.add(tuple(sorted(current_triplet)))
+                if (i == j):
+                    j += 1
+                    continue
+                elif i == k:
+                    k -= 1
+                    continue
+                elif j == k:
+                    break
+                # sum of triplets
+                triplet_sum = sum(current_triplet)
+
+                if triplet_sum == 0:
+                    all_triplets.add(tuple(sorted(current_triplet)))
+                elif triplet_sum > 0:
+                    k -= 1
+                elif triplet_sum < 0:
+                    j += 1
+
         return all_triplets
+
+
+# My soltuion time compexity:
+# O(n^3)
+
+# from typing import List
+
+
+# class ThreeSum:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         # set to store triplets
+#         all_triplets = set()
+
+#         # iterate through the list
+#         for i in range(len(nums)):
+#             for j in range(i):
+#                 for k in range(j):
+
+#                     # if indicies are same skip the iteration
+#                     if (i == j or i == k or j == k):
+#                         continue
+
+#                     # list of current triplets
+#                     current_triplet = [nums[i], nums[j], nums[k]]
+
+#                     # if sum of the triplet == 0, add to the set
+#                     if sum(current_triplet) == 0:
+#                         all_triplets.add(tuple(sorted(current_triplet)))
+#         return all_triplets
